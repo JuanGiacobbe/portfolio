e89c279b38793ecda88166945ea02d6ea74350e5
Juan_Giacobbe <jgiacobbe32@gmail.com>
2022-03-16 14:56:14 -0300

    landing page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,8 @@
 import ContactForm from './ContactForm';
 import './Contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 
 
 export default function Contact() {
@@ -10,19 +10,29 @@ export default function Contact() {
         <div className='contact' id="contact">
             <div className='contact-grid'>
                 <div className='contact-container'>
-                    <h2 className='section-sub'>get in touch.</h2>
+                    <h2 className='section-sub'>Contact me</h2>
                     <p className='subsection-des'>you can leave a message in the
                     form in rigth; or you can contact me in my social media in
                     Github and Instagram clicking in the icons bellow.  </p>
 
-                    <div className='icons'>
-                        <a href='instagram.com/juan_giacobbe_' target='_blank'><FontAwesomeIcon  className='icon' icon={faInstagram} /></a>
-                        <FontAwesomeIcon className='icon' icon={faGithub} />
-                    </div>
+                    <ContactForm />
+                    
                 </div>
                 
-                <div className='form'>
-                    <ContactForm />
+                <div className='data-column'>
+                    <h3 className='data-title'>Email:</h3>
+                    <h3 className='personal-data'>juangiacobbe@gmail.com</h3>
+                    <h3 className='data-title'>Number:</h3>
+                    <h3 className='personal-data'>123456789</h3>
+
+                    <div className='icons-container'>
+                        <h3 className='data-title'>Social media:</h3>
+
+                        <div className='icons'>
+                            <FontAwesomeIcon className='icon' icon={faInstagramSquare} />
+                            <FontAwesomeIcon className='icon' icon={faGithubSquare} />
+                        </div>
+                    </div>
                 </div>
             </div>            
         </div>
--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
+// importing styles
 import './ContactForm.css';
 
 
 export default function ContactForm() {
-    const[name, setName] = useState(false);
-    const[mail, setMail] = useState(false);
-
-    const activeName = () => {
-        if(name === false) {
-            setName(true);
-            setMail(false);
-        }
-    }
-    const activeMail = () => {
-        if(mail === false) {
-            setMail(true);
-            setName(false);
-        }
-    }
-
     return (
-        <form className='form'>
-            <input type='text' className={`input ? ${name ? 'name-active' : ''}`} onClick={activeName} placeholder='your name'/>
-            <input type='email' className={`input ? ${mail ? 'mail-active' : ''}`} onClick={activeMail} placeholder='your email'/>
-            <textarea type='text' className='msg' placeholder='message'/>
-            <input type='submit' className='btn submit' value='submit'/>
+        <div className='contact-form'>
+            <form className='form'>
+                <div className='data-input'>
+                    <div>
+                    <label for='#name' className='label' >Name: </label>
+                    <input type='text' id='#name' className='input name' required />
+                    </div>
+                    <div>
+                    <label for='#mail' className='label'>Email: </label>
+                    <input type='email' id='name' className='input email' required />
+                    </div>
+                </div>
+                    <div>
 
-        </form>
+                    <label for='#msg' className='label'>Message: </label>
+                    <textarea type='text' id='msg' className='input msg' required />
+                    </div>
+
+                <input type='submit' className='btn submit' value='send'/>
+
+            </form>
+        </div>
     );
 }
